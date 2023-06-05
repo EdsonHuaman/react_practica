28d832636b27d547f9ab60c91f29fa7912fc4354
Dhenis <dhenis@mail.com>
2023-06-05 15:45:19 -0500

    guardar cambios

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
 import React from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList'; 
-import { CreateTodoButton} from './CreateTodoButton'
+import { TodoList } from './TodoList';
+import { CreateTodoButton } from './CreateTodoButton';
 import { TodoItem } from './TodoItem';
 
-
 const defaultTodos = [
-  { text: 'Cortar pollo con plumas xd', completed: true},
-  { text: 'Plantillan en React.js', completed: false},
-  { text: 'Lloar por el amor de ella xd', completed: false},
-  { text: 'C mamó...', completed: false},
+  { text: 'Cortar pollo con plumas xd', completed: true },
+  { text: 'Plantillan en React.js', completed: false },
+  { text: 'Lloar por el amor de ella xd', completed: false },
+  { text: 'C mamó...', completed: false },
+  { text: 'Los bricheros', completed: true },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLocaleLowerCase();
+    const searchText = searchValue.toLocaleLowerCase();
+    return todoText.includes(searchText);
+  });
+
+  // const searchedTodos = todos.filter((todo) => {
+  //  return todo.text.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())
+  // });
+
+  console.log('Los usuarios buscan todos de ' + searchValue);
+
   return (
     <>
-
-
-      <TodoCounter completed={16} total={25}/>
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-         {defaultTodos.map(todo => (
-          <TodoItem key={todo.text} 
-          text={todo.text}
-          completed={todo.completed} />
-          ))}
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
 
-     <CreateTodoButton />
+      <CreateTodoButton />
     </>
   );
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,17 +1,17 @@
+import React from 'react';
 import './TodoSearch.css';
-function TodoSearch() {
-    return (
-        <input
-         placeholder="Cortar Carne" 
-          className="TodoSearch"
-          onChange={(event) => {
-            console.log('Escribiste en el TodoSearch');
-            console.log(event);
-            console.log(event.target);
-            console.log(event.target.value);
-          }}
-        />
-      );
-   }
 
-   export { TodoSearch };
+function TodoSearch({ searchValue, setSearchValue }) {
+  return (
+    <input
+      placeholder='Cortar Carne'
+      className='TodoSearch'
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
+    />
+  );
+}
+
+export { TodoSearch };
